da5006ad11871efa0948e5499c05d493fc8cb4dc
scarl-ett <chdnjs7610@gmail.com>
2022-04-25 22:22:51 +0900

    router

---
+++ src/pages/NotFound.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 64,
+        position: 'absolute',
+        width: 'absolute',
+        height: '100%',
+      }}
+    >
+      404
+    </div>
+  );
+};
+
+export default NotFound;
